bd862aeac03cb27797411720e4807d5a8dbc1188
Елена <lenna@gmail.com>
2023-08-27 19:06:25 +0300

    Added program in Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное число M ");
+int number_M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N");
+int number_N = Convert.ToInt32(Console.ReadLine());
+if (number_M < 0 || number_N < 0)
+{
+    Console.WriteLine("Вы ввели ненатуральное число");
+    return;
+}
+int sum_Elem = SumDigitRange(number_M, number_N);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N = {sum_Elem}");
+
+int SumDigitRange(int m, int n)
+{
+
+    if (m == n) return m;
+
+    return m + SumDigitRange(m + 1, n);
+}
+
